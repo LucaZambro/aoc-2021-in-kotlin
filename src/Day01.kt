2ab80fe78015373e02b51fd7e4280a792104b863
LucaZambro <lucazambro46@hotmail.it>
2021-12-03 11:38:29 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+
+        var prev = 0
+        input.forEach {
+            if (prev != 0) {
+                if (it.toInt() > prev)
+                    count++
+            } else prev = it.toInt()
+            prev = it.toInt()
+        }
+
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var count = 0
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val windows = input.windowed(3)
+        var prevSum = 0
+        windows.forEach { window ->
+            val sum = window.sumOf { it.toInt() }
+            if (prevSum != 0) {
+                if (sum > prevSum)
+                    count++
+            }
+            prevSum = sum
+        }
+
+        return count
+    }
 
     val input = readInput("Day01")
     println(part1(input))
